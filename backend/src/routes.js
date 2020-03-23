a2f58464456a485f2f970aa6a99624817b076d9c
kelvuynsantana <santanakelvyn@gmail.com>
2020-03-22 23:03:41 -0300

    Add Avatar to users

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,17 @@
+/** Import Modules */
 import { Router } from 'express';
+import multer from 'multer';
+/** Import Controllers */
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-
+import FileController from './app/controllers/FileController';
+/** Import Configs */
+import multerConfig from './config/multer';
+/** Import Middlwares */
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 /** Insert your routes here */
 routes.post('/users', UserController.store);
@@ -12,4 +19,5 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
